8e5c502070a716a3fcca91df73291769bbb852ec
aamcodes <aaronmendezcodes@gmail.com>
2023-02-26 23:43:20 -0500

    Finished tests for bags

--- Bag.test.js
+++ Bag.test.js
@@ -1,13 +1,17 @@
-const Bag = require('./Bag')
+const Bag = require('./Bag');
 
 describe('Bag class', () => {
-    //bag has weight
-    test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
-    })
-    //if bag has no weight, return error
-    test('bag has no weight to return an error', () => {
-        expect(() => new Bag()).toThrowError('bag needs weight')
-    })
-})
+	//bag has weight
+	test('bag has weight', () => {
+		const bag = new Bag(22);
+		expect(bag.weight).toBe(22);
+	});
+	//if bag has no weight, return error
+	test('bag has no weight to return an error', () => {
+		expect(() => new Bag()).toThrowError('bag needs weight');
+	});
+
+	test('bag does not exceed max weight', () => {
+		expect(() => new Bag(25)).toThrow('weight is too heavy');
+	});
+});
